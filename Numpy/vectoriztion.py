3ff2f2c22ab1850c185ac690bb6c6aea0417b2a3
Muhammad-1Haseeb <muhammadhaseeb24@gmail.com>
2026-08-31 02:11:24 +0500

    learning numpy with chai aur code

--- Numpy/vectoriztion.py
+++ Numpy/vectoriztion.py
@@ -54,13 +54,63 @@ import numpy as np
 # print(result)
 # print(result.shape)
 
-marks = np.array([65, 72, 81, 90, 55])
-bonus = np.array([5, 10]).reshape(2, 1)
+# marks = np.array([65, 72, 81, 90, 55])
+# bonus = np.array([5, 10]).reshape(2, 1)
 
-result = marks + bonus
+# result = marks + bonus
 
-print(bonus)
-print(bonus.shape)
+# print(bonus)
+# print(bonus.shape)
 
-print(result)
-print(result.shape)
+# print(result)
+# print(result.shape)
+
+# marks = np.array([
+#     [70, 80, 90],
+#     [60, 75, 85],
+#     [88, 92, 79]
+# ])
+
+# bonus = np.array([5,2,10])
+
+# final_marks = marks + bonus
+# print(f"Final Marks: {final_marks}")
+# print(f"Final marks Shape: {final_marks.shape}")
+
+
+# sales = np.array([
+#     [100, 120, 110],
+#     [80,  95,  105],
+#     [150, 140, 160],
+#     [90,  100, 115]
+# ])
+
+# bonus_rate = np.array([0.10, 0.05, 0.20])
+# bonus_amount = sales * bonus_rate
+
+# print(bonus_amount)
+# print(bonus_amount.shape)
+
+# marks = np.array([
+#     [70, 80, 90],
+#     [60, 75, 85],
+#     [88, 92, 79],
+#     [95, 87, 91]
+# ])
+
+# student_averages = np.mean(marks, axis=0)
+# print(student_averages)
+# print(student_averages.shape)
+
+
+marks = np.array([
+    [70, 80, 90],
+    [60, 75, 85],
+    [88, 92, 79],
+    [95, 87, 91]
+])
+student_averages = np.mean(marks, axis=1)
+
+excellent = student_averages >= 80
+excellent_students = student_averages[excellent]
+print(excellent_students)
